47ca1da08f998431ccc9346166398786f77e98fa
sylyon <sylyon@iu.edu>
2016-04-30 16:27:45 -0400

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+// Sydney Lyon
+// first Java program on linux command line
+
+class HelloWorld{
+
+    public static void Hello(){
+	System.out.println("Hello, world!");
+    }
+
+    public static void main(String[] args){
+	Hello();
+    }
+    
+}
